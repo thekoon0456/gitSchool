a7f5ea57f310adefef7acf581a130d5b5e3f0896
thekoon0456 <thekoon0456@gmail.com>
2022-12-02 10:24:28 +0900

    hi

---
+++ HelloWorld/HelloWorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWorldApp.swift
+//  HelloWorld
+//
+//  Created by Deokhun KIM on 2022/10/07.
+//
+
+import SwiftUI
+
+@main
+struct HelloWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
